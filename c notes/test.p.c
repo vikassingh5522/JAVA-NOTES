88256e611504b4082b2b2d874de1fc333c34ac0f
vikas singh <135737417+vikassingh5522@users.noreply.github.com>
2025-04-03 20:16:31 +0530

    Add files via upload

---
+++ c notes/test.p.c
@@ -0,0 +1,7 @@
+
+// Given a rotated sorted array, find if there exists a pair whose sum equals the target.
+// Input: Array [11, 15, 6, 8, 9, 10], Target 16.
+// Output: True (pair [6, 10] exists).
+
+#include <stdio.h>
+
